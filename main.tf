232056043f3b11986b641cf2be9965e20bd0d668
SATEESH <119327085+sateesh274@users.noreply.github.com>
2024-12-12 13:05:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAQXPZC5TWJVKSLCGH"
+  secret_key = "11g7KIhYAP9UJh3kpI25ZOjlA+ifpCXs2lds8WAI"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0614680123427b75e"
+  instance_type = var.instance_type
+  tags= {
+    Name = "sateesh"
+  }
+}
